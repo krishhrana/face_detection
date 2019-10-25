aa0d0c6180ed9916903a3147f995f20d8d0fab26
Krish Rana <51706825+krishhrana@users.noreply.github.com>
2019-10-25 19:25:14 +0530

    Add files via upload

---
+++ face_detection.py
@@ -0,0 +1,59 @@
+# import libraries
+import cv2
+import face_recognition
+# Get a reference to webcam 
+video_capture = cv2.VideoCapture("./test2.mov")
+
+image = face_recognition.load_image_file("./mkbhd.jpg")
+face_encoding = face_recognition.face_encodings(image)[0]
+
+known_faces = [
+face_encoding,
+]
+# Initialize variables
+face_locations = []
+face_encodings = []
+face_names = []
+
+while True:
+    # Grab a single frame of video
+    ret, frame = video_capture.read()
+
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_frame = frame[:, :, ::-1]
+
+    # Find all the faces in the current frame of video
+    face_locations = face_recognition.face_locations(rgb_frame)
+    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+    
+    face_names = []
+    for face_encoding in face_encodings:
+        # See if the face is a match for the known face(s)
+        match = face_recognition.compare_faces(known_faces, face_encoding, tolerance=0.50)
+
+        name = None
+        if match[0]:
+            name = "MKBHD"
+
+        face_names.append(name)
+
+    # Display the results
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        
+        cv2.rectangle(frame, (left, bottom - 25), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.5, (255, 255, 255), 1)
+
+
+    # Display the resulting image
+    cv2.imshow('Video', frame)
+
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()
